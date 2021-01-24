d7a16545bce292f91c61b05bdfd4b837b64274eb
Danny-HypnoTank <d.pymhember@aol.co.uk>
2021-01-24 17:42:25 +0000

    removed test files, updated sample scene
    tested painting and it works - just gotta figure out how to get it to work with URP

--- Assets/Scripts/Player/PlayerBase.cs
+++ Assets/Scripts/Player/PlayerBase.cs
@@ -29,17 +29,30 @@ public class PlayerBase : MonoBehaviour
     private float dashTimer = 0;
     private readonly float activationValue = 0.15f;
 
+    //Splat Variables
+    private Vector4 channelMask = new Vector4(1, 0, 0, 0);
+
+    private int splatsX = 1;
+    private int splatsY = 1;
+    [SerializeField]
+    private float splatScale = 1.0f;
+    [SerializeField]
+    private MeshRenderer playerMeshRend;
+    [SerializeField]
+    private Material[] colourMaterials;
+
 
     public Rigidbody Rigid { get; private set; }
-    public PlayerInput Input { get; private set; }
+    public PlayerInput PlayerInput { get; private set; }
     public StateMachine<PlayerBase> StateMachine { get; private set; }
     public Vector2 MovementValue { get; private set; }
 
     private void Awake()
     {
         Rigid = GetComponent<Rigidbody>();
-        Input = GetComponent<PlayerInput>();
+        PlayerInput = GetComponent<PlayerInput>();
         StateMachine = new StateMachine<PlayerBase>(this);
+        playerMeshRend.material = colourMaterials[0];
     }
 
     private void Update()
@@ -47,7 +60,41 @@ public class PlayerBase : MonoBehaviour
         StateMachine.Update();
         DashCharging();
         Dash();
+        ColourChanger();
     }
+
+    private void ColourChanger()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) //orange/yellow
+        {
+            channelMask = new Vector4(1, 0, 0, 0);
+            playerMeshRend.material = colourMaterials[0];
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2)) //red
+        {
+            channelMask = new Vector4(0, 1, 0, 0);
+            playerMeshRend.material = colourMaterials[1];
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3)) //green
+        {
+            channelMask = new Vector4(0, 0, 1, 0);
+            playerMeshRend.material = colourMaterials[2];
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4)) //blue
+        {
+            channelMask = new Vector4(0, 0, 0, 1);
+            playerMeshRend.material = colourMaterials[3];
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Splat();
+        }
+    }
+
 
     private void OnMove(InputValue inputValue)
     {
@@ -116,14 +163,42 @@ public class PlayerBase : MonoBehaviour
 
     private void Splat()
     {
+        //Get how many splats are in the splat atlas
+        splatsX = SplatManagerSystem.instance.splatsX;
+        splatsY = SplatManagerSystem.instance.splatsY;
+
         Ray ray = new Ray(transform.position, -transform.up);
 
         if (Physics.Raycast(ray, out RaycastHit hit))//, scoreLayer))
         {
             if (hit.collider.CompareTag("Paintable"))
             {
+                Vector3 leftVec = Vector3.Cross(hit.normal, Vector3.up);
+                float randScale = Random.Range(0.5f, 1.5f);
 
+                GameObject newSplatObject = new GameObject();
+                newSplatObject.transform.position = hit.point;
+                if (leftVec.magnitude > 0.001f)
+                {
+                    newSplatObject.transform.rotation = Quaternion.LookRotation(leftVec, hit.normal);
+                }
+                newSplatObject.transform.RotateAround(hit.point, hit.normal, Random.Range(-180, 180));
+                newSplatObject.transform.localScale = new Vector3(randScale, randScale * 0.5f, randScale) * splatScale;
 
+                Splat newSplat;
+                newSplat.splatMatrix = newSplatObject.transform.worldToLocalMatrix;
+                newSplat.channelMask = channelMask;
+
+                float splatscaleX = 1.0f / splatsX;
+                float splatscaleY = 1.0f / splatsY;
+                float splatsBiasX = Mathf.Floor(Random.Range(0, splatsX * 0.99f)) / splatsX;
+                float splatsBiasY = Mathf.Floor(Random.Range(0, splatsY * 0.99f)) / splatsY;
+
+                newSplat.scaleBias = new Vector4(splatscaleX, splatscaleY, splatsBiasX, splatsBiasY);
+
+                SplatManagerSystem.instance.AddSplat(newSplat);
+
+                GameObject.Destroy(newSplatObject);
             }
         }
     }
